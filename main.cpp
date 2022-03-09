e56d5c04008c9873c9a3a3d66932c54c5b832b09
Aleksandr <achekmarev@194-67-109-243.cloudvps.regruhosting.ru>
2022-03-09 18:08:53 +0300

    Написал функцию fib

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
-#include <iotsream> 
+#include <iostream>
 
-using namesapce std;
+using namespace std;
 
-int main(){
-cout << "Hallo, World!";
-return 0;
+int fib(int x){
+    if (x==1) return 1;
+    if (x==2) return 1;
+    if (x>2) return (fib(x-2)+fib(x-1));
+}
+
+int main()
+{
+    int a;
+    cin >> a;
+    cout << fib(a) << endl;
+
+    return 0;
 }
